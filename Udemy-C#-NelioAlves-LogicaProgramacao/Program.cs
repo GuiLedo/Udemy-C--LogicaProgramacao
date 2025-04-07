f1f9c89e9c3eb44e3f86a23f6f92c291856f2834
Guilherme Ledo <guilhermeledochagas@gmail.com>
2025-04-06 21:04:48 -0300

    first exercises

--- Udemy-C#-NelioAlves-LogicaProgramacao/Program.cs
+++ Udemy-C#-NelioAlves-LogicaProgramacao/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace cursoUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double baseRet, heightRet, areaRet, perimeterRet, diagonalRet;
+
+            Console.WriteLine("Qual a base do retângulo?");
+            baseRet = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual a altura do retângulo?");
+            heightRet = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            areaRet = baseRet * heightRet;
+            Console.WriteLine("Área do retângulo: " + areaRet.ToString("F4"));
+            perimeterRet = baseRet * 2 + heightRet * 2;
+            Console.WriteLine("Perímetro do retângulo: " + perimeterRet.ToString("F4"));
+            diagonalRet = Math.Sqrt((Math.Pow(baseRet, 2) + Math.Pow(heightRet, 2)));
+            Console.WriteLine("Diagonal do retângulo: " + diagonalRet.ToString("F4"));
+
+        }
+    }
+}
