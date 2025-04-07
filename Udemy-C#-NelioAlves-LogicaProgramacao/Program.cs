754a9490d7ef1e854d7d6cf59467437ff87b9423
Guilherme Ledo <guilhermeledochagas@gmail.com>
2025-04-06 21:24:53 -0300

    first exercises2

--- Udemy-C#-NelioAlves-LogicaProgramacao/Program.cs
+++ Udemy-C#-NelioAlves-LogicaProgramacao/Program.cs
@@ -11,9 +11,9 @@ namespace cursoUdemy
             double baseRet, heightRet, areaRet, perimeterRet, diagonalRet;
 
             Console.WriteLine("Qual a base do retângulo?");
-            baseRet = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            baseRet = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine("Qual a altura do retângulo?");
-            heightRet = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            heightRet = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             areaRet = baseRet * heightRet;
             Console.WriteLine("Área do retângulo: " + areaRet.ToString("F4"));
             perimeterRet = baseRet * 2 + heightRet * 2;
