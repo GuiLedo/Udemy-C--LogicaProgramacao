e1346a320cc4f2a1fbde57cd12d24852c563fafd
Guilherme Ledo <guilhermeledochagas@gmail.com>
2025-04-09 23:03:47 -0300

    exercicio notas

--- Udemy-C#-NelioAlves-LogicaProgramacao/Program.cs
+++ Udemy-C#-NelioAlves-LogicaProgramacao/Program.cs
@@ -7,24 +7,26 @@ namespace cursoUdemy
     {
         static void Main(string[] args)
         {
-            string nome1, nome2;
-            int idade1, idade2;
-            double idadeMedia;
+            double n1, n2, nf;
 
-            Console.WriteLine("Favor fornecer nome e idade da P1:");
-            string[] vet1 = Console.ReadLine().Split(' ');
-            nome1 = vet1[0];
-            idade1 = int.Parse(vet1[1]);
+            Console.WriteLine("Nota 1º Semestre:");
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Favor fornecer nome e idade da P2:");
-            string[] vet2 = Console.ReadLine().Split(' ');
-            nome2 = vet2[0];
-            idade2 = int.Parse(vet2[1]);
+            Console.WriteLine("Nota 2º Semestre:");
+            n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Nomes: " + vet1[0] + " e " + vet2[0]);
-            idadeMedia = (double) (idade1 + idade2) / 2.0;
-            Console.WriteLine("Idade Média: " + idadeMedia);
+            nf = n1 + n2;
 
+            Console.WriteLine("NOTA FINAL = " + nf.ToString("F2", CultureInfo.InvariantCulture));
+            if (nf >= 60.0)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado!");
+            }
+            Console.ReadLine(); 
         }
     }
 }
